2e5b45832a5055c20bd14baaa8e3441e685b1f7d
AlexandraCastro <castroyenny74@gmail.com>
2026-09-04 22:56:32 -0500

    Primer commit

---
+++ public/assets/js/public/registro.js
@@ -0,0 +1,10 @@
+        // Lógica para mostrar/ocultar contraseña
+        const togglePassword = document.getElementById('togglePassword');
+        const passwordInput = document.getElementById('password');
+
+        togglePassword.addEventListener('click', () => {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            togglePassword.classList.toggle('fa-eye');
+            togglePassword.classList.toggle('fa-eye-slash');
+        });
